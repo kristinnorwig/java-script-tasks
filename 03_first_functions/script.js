4f9a8c1b1f5283f873fc51b053514c3736a8985b
Kristin Norwig <kristin.norwig@gmail.com>
2023-10-19 11:06:48 +0200

    task first functions

---
+++ 03_first_functions/script.js
@@ -0,0 +1,29 @@
+//Aufgabe 1
+
+function welcomeMsg(Name) {
+  return "Welcome " + Name;
+}
+
+console.log(welcomeMsg("Jane"));
+
+//Aufgabe 2
+
+function calcGrossPrice(a, b) {
+  return a + a * b;
+}
+
+console.log(calcGrossPrice(20, 0.19));
+
+console.log(calcGrossPrice(40, 0.16));
+
+//Aufgabe 3
+
+function addPositive(a, b) {
+  return Math.abs(a) + Math.abs(b);
+}
+
+console.log(addPositive(2, 3));
+
+console.log(addPositive(3, -5));
+
+console.log(addPositive(-1, -8));
