756c99a8f446f2c4e6bb537d049b8b23dfa3d67b
Kristin Norwig <kristin.norwig@gmail.com>
2023-10-23 17:43:40 +0200

    feat: adds alternative solutions

--- 06_using_loops/script.js
+++ 06_using_loops/script.js
@@ -22,16 +22,17 @@ function oddNumbers(firstNmbr, secondNmbr) {
 }
 
 console.log(oddNumbers(0, 4));
-// result should be: 1,3
+// result: 1,3
 
 console.log(oddNumbers(10, 33));
-// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+// result: 11,13,15,17,19,21,23,25,27,29,31,33
 
 console.log(oddNumbers(9, 12));
-// result should be: 9,11
+// result: 9,11
 
-/*Aufgabe 1 Alternative
-// Contract:
+// Aufgabe 1 Alternative 1
+
+//Contract:
 // two parameters: lower: number, upper: number
 // lower > 0;
 // upper > lower
@@ -39,24 +40,63 @@ console.log(oddNumbers(9, 12));
 // result: number[] - n in result -> n >= lower, n <= upper,
 // result encoded as string, with numbers n separated by ','
 
- function oddNumbers(lower, upper) { // 3, 5
-	let result = '';
-	if (lower % 2 === 0) {
-		lower = lower + 1;
-	}
-	for (let n = lower; n <= upper; n += 2) { // 1, 2, 3
-		// 1. consider odd numbers only
+function oddNumbers(lower, upper) {
+  // 3, 5
+  let result = "";
+  if (lower % 2 === 0) {
+    lower = lower + 1;
+  }
+  for (let n = lower; n <= upper; n += 2) {
+    // 1, 2, 3
+    // 1. consider odd numbers only
 
-		// 2. add the odd number to the result string, comma seperated
-		if (result === '') { // first iteration
-			result = result + n;
-		} else {
-			result = result + ',' + n;
-		}
-		// let's detect if we're at the begining of the string
-	}
-	return result;
-} */
+    // 2. add the odd number to the result string, comma seperated
+    if (result === "") {
+      // first iteration
+      result = result + n;
+    } else {
+      result = result + "," + n;
+    }
+    // let's detect if we're at the begining of the string
+  }
+  return result;
+}
+
+console.log(oddNumbers(0, 4));
+
+console.log(oddNumbers(10, 33));
+
+console.log(oddNumbers(9, 12));
+
+//Aufgabe 1 Alternative 2
+
+function oddNumbers(n1, n2) {
+  let result = "";
+  //prüfen, ob Zahl negativ, wenn nicht, dann fortfahren, wenn ja dann return
+  if (n1 < 0 || n2 < 0) {
+    return "negative numbers are not allowed";
+  }
+
+  for (let index = n1; index <= n2; index++) {
+    //index zählt in der Schleife immer nach oben, ist dann also "aktuelle" Zahl
+    if (index % 2 === 1) {
+      // Alternativ: index % 2 !== 0
+      if (result === "") {
+        //Komma bei der ersten Zahl entfernen
+        result = index;
+      } else {
+        result += "," + index;
+      } // Alternativ: result = result + index
+    }
+  }
+  return result;
+}
+
+console.log(oddNumbers(0, 4));
+
+console.log(oddNumbers(10, 33));
+
+console.log(oddNumbers(9, 12));
 
 // Aufgabe 2 Implement the charCount function
 
@@ -78,10 +118,32 @@ function charCount(word, char) {
 }
 
 console.log(charCount("hello", "l"));
-// result should be: 2
+// result: 2
 
 console.log(charCount("mama", "m"));
-// result should be: 2
+// result: 2
 
 console.log(charCount("Resümee", "e"));
-// result should be: 3
+// result: 3
+
+// Aufgabe 2 Alternative 1
+function charCount(word, character) {
+  let count = 0; //null, weil es eine number ist und kein string wie bei Aufg. 1
+  if (character.length !== 1) {
+    // Alternativ zu !== geht auch >1
+    return;
+  }
+  for (let index = 0; index < word.length; index++) {
+    if (word[index].toUpperCase() === character.toUpperCase()) {
+      //[] eckige Klammern fragen die Buchstaben in Variable "word" ab, .toUpperCase() ist egal ob Groß- und Kleinschreibung, "man macht alles groß"
+      count++;
+    } //Alternativ: count = count + 1
+  }
+  return count;
+}
+
+console.log(charCount("hello", "l"));
+
+console.log(charCount("mama", "m"));
+
+console.log(charCount("Resümee", "e"));
