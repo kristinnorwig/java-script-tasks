f17096384e0df465e8526cac33256a2884b26a9d
Kristin Norwig <kristin.norwig@gmail.com>
2023-11-08 14:16:25 +0100

    feat: adds tasks filter-api-data

--- 11_recap_day/filter-api-data/filter-api-data.js
+++ 11_recap_day/filter-api-data/filter-api-data.js
@@ -1,3 +1,24 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  const result = [];
+
+  for (let objects of apiData) {
+    // Variable zur Abfrage definiert, Variable auf true gesetzt, damit in zweiter For-Schleife das hasKey auf false überprüft werden kann
+    let hasKey = true;
+    for (let key of mandatoryKeys) {
+      // Ausrufezeichen vor Bedingung fragt ob Bedingung nicht zutrifft
+      if (!objects.hasOwnProperty(key)) {
+        hasKey = false;
+      }
+    }
+
+    //
+    if (hasKey) {
+      result.push(objects);
+    }
+  }
+  return result;
 }
+
+// apiData = Array mit Objekten, Objekte können versch. Keys haben
+// mandatoryKeys = Array mit den Keys als String die auf jeden Fall in den Objekte von apiData
+// Ergebnis soll Array mit Objekten sein die nur die Objekte haben die alle Keys von mandatoryKeys von erfüllen (wie Filter
